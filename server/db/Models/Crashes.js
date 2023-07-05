38104fbd824e69c835fac5111dbb04223f960466
Gabe <gabrielarce3@gmail.com>
2023-07-04 23:59:35 -0700

    connect to mongo db with mongoose

---
+++ server/db/Models/Crashes.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const crashSchema = new mongoose.Schema(
+  {
+    COLLISION_DATE: Date,
+    COLLISION_TIME: String,
+    DAY_OF_WEEK: Number,
+    INTER: String,
+    WEATHER: String,
+    STATE_HWY_IND: String,
+    COLLISION_SEVERITY: Number,
+    NUMBER_KILLED: Number,
+    NUMBER_INJURED: Number,
+    PARTY_COUNT: Number,
+    PRIMARY_COLL_FACTOR: String,
+    HIT_AND_RUN: String,
+    TYPE_OF_COLLISION: String,
+    PED_ACTION: String,
+    ROAD_COND_1: String,
+    LIGHTING: String,
+    PEDESTRIAN_ACCIDENT: String,
+    POINT_X: { type: Number, double: true, require: true },
+    POINT_Y: { type: Number, double: true, require: true },
+  },
+  { collection: 'Crashes' }
+);
+
+module.exports = mongoose.model('Crash', crashSchema);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const db = require('./db/config');
+const connectDB = require('./db/config');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+connectDB();
+const Crash = require('./db/Models/Crashes');
 
 // Middleware
 app.use(express.json());
@@ -16,16 +19,15 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('/api', async (req, res) => {
   try {
     // Execute a simple query to check the connection
-    const result = await db.any(
-      'SELECT * FROM accidents WHERE number_killed >= 1;'
-    );
-    console.log('Connected to the database:');
-    res.json(result);
+    let results = await Crash.find({
+      NUMBER_KILLED: {
+        $gte: 1,
+      },
+    });
+    console.log(results);
+    res.json(results);
   } catch (error) {
     console.error('Error connecting to the database:', error);
-  } finally {
-    // Close the database connection
-    db.$pool.end();
   }
 });
 
