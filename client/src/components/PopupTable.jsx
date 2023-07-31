11245305ac0850adc215b8f32de1b9a698f92ade
Gabe <gabrielarce3@gmail.com>
2023-07-30 17:26:41 -0700

    add street view link to popup component

--- client/src/components/PopupTable.jsx
+++ client/src/components/PopupTable.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
+const dayOfWeek = (day) => {
+  switch (day) {
+    case 1:
+      return 'Sunday';
+    case 2:
+      return 'Monday';
+    case 3:
+      return 'Tuesday';
+    case 4:
+      return 'Wednesday';
+    case 5:
+      return 'Thursday';
+    case 6:
+      return 'Friday';
+    case 7:
+      return 'Saturday';
+    default:
+      return 'Invalid day';
+  }
+};
+
 const PopupTable = ({ data }) => {
   return (
     <div className="max-w-md mx-auto p-2 bg-white shadow-md rounded-md">
@@ -10,13 +31,27 @@ const PopupTable = ({ data }) => {
           {data.COLLISION_DATE.split('T')[0]}{' '}
         </span>
       </div>
-      <div className="my-0 flex justify-between">
+      <div className="my-1 flex justify-between">
         <span className="text-lg font-semibold my-0">Time: </span>
         <span className="text-lg font-normal"> {data.COLLISION_TIME} </span>
       </div>
-      <div className="my-0 flex justify-between">
+      <div className="my-1 flex justify-between">
         <span className="text-lg font-semibold my-0">Day of Week: </span>
-        <span className="text-lg font-normal"> {data.DAY_OF_WEEK} </span>
+        <span className="text-lg font-normal">
+          {' '}
+          {dayOfWeek(data.DAY_OF_WEEK)}{' '}
+        </span>
+      </div>
+      <div className="my-1 flex justify-between">
+        <span className="text-base  my-0">
+          <a
+            href={`http://maps.google.com/maps?q=&layer=c&cbll=${data.POINT_Y},${data.POINT_X}&cbp=11,0,0,0,0`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Street View
+          </a>{' '}
+        </span>
       </div>
     </div>
   );
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { LiaBeerSolid } from 'react-icons/fa';
+import { FaBeer, FaRoad } from 'react-icons/fa';
 
 export default function Sidebar({ crashes, setFiltered }) {
   const [open, setOpen] = useState(false);
@@ -54,16 +54,7 @@ export default function Sidebar({ crashes, setFiltered }) {
                   onClick={handleFilterChange}
                   className="flex items-center p-2 space-x-3 rounded-md"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-gray-100"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path d="M32 64c0-17.7 14.3-32 32-32H352c17.7 0 32 14.3 32 32V96h51.2c42.4 0 76.8 34.4 76.8 76.8V274.9c0 30.4-17.9 57.9-45.6 70.2L384 381.7V416c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V64zM384 311.6l56.4-25.1c4.6-2.1 7.6-6.6 7.6-11.7V172.8c0-7.1-5.7-12.8-12.8-12.8H384V311.6zM160 144c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144zm64 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144zm64 0c0-8.8-7.2-16-16-16s-16 7.2-16 16V368c0 8.8 7.2 16 16 16s16-7.2 16-16V144z" />
-                  </svg>{' '}
+                  <FaBeer />
                   <span className="text-gray-100"> Alcohol Involved </span>{' '}
                 </button>{' '}
               </li>{' '}
