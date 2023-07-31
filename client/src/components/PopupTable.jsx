98941d9ed246c02a8751ed59682fb78410a36fbd
Gabe <gabrielarce3@gmail.com>
2023-07-30 19:48:54 -0700

    fix day to week conversion function

--- client/src/components/PopupTable.jsx
+++ client/src/components/PopupTable.jsx
@@ -3,41 +3,58 @@ import React from 'react';
 const dayOfWeek = (day) => {
   switch (day) {
     case 1:
-      return 'Sunday';
-    case 2:
       return 'Monday';
-    case 3:
+    case 2:
       return 'Tuesday';
-    case 4:
+    case 3:
       return 'Wednesday';
-    case 5:
+    case 4:
       return 'Thursday';
-    case 6:
+    case 5:
       return 'Friday';
-    case 7:
+    case 6:
       return 'Saturday';
+    case 7:
+      return 'Sunday';
     default:
       return 'Invalid day';
   }
 };
 
+function convertToAmPmTime(militaryTime) {
+  let [hours, minutes] = militaryTime.split(':').map(Number);
+  let period = 'AM';
+
+  if (hours >= 12) {
+    period = 'PM';
+    if (hours > 12) {
+      hours -= 12;
+    }
+  }
+
+  return `${hours}:${String(minutes).padStart(2, '0')} ${period}`;
+}
+
 const PopupTable = ({ data }) => {
   return (
     <div className="max-w-md mx-auto p-2 bg-white shadow-md rounded-md">
       <div className="my-0 flex justify-between">
-        <span className="text-lg font-semibold my-0">Crash Date: </span>
-        <span className="text-lg font-normal">
+        <span className="text-lg font-semibold">Crash Date: </span>
+        <span className="text-lg font-normal ml-3">
           {' '}
           {data.COLLISION_DATE.split('T')[0]}{' '}
         </span>
       </div>
       <div className="my-1 flex justify-between">
         <span className="text-lg font-semibold my-0">Time: </span>
-        <span className="text-lg font-normal"> {data.COLLISION_TIME} </span>
+        <span className="text-lg font-normal">
+          {' '}
+          {convertToAmPmTime(data.COLLISION_TIME)}{' '}
+        </span>
       </div>
       <div className="my-1 flex justify-between">
         <span className="text-lg font-semibold my-0">Day of Week: </span>
-        <span className="text-lg font-normal">
+        <span className="text-lg font-normal ml-3">
           {' '}
           {dayOfWeek(data.DAY_OF_WEEK)}{' '}
         </span>
