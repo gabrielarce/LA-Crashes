e517ba45fe3ec56bf2673a811310aaeaad3ac005
Gabe <gabrielarce3@gmail.com>
2024-01-20 22:37:55 -0800

    edit package json

--- server/db/config.js
+++ server/db/config.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      'mongodb+srv://gabrielarce:C0VSX1Nr0p8Hy7NT@la-crashes-2022.xarb55r.mongodb.net/LA-Crashes-2022?retryWrites=true&w=majority',
-      {
-        dbName: 'LA-Crashes-2022',
-      }
-    );
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      dbName: 'LA-Crashes-2022',
+    });
   } catch (err) {
     console.error(err);
     process.exit(1);
